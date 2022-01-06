c16eda03764414c003caa2c57d047df45bc964ec
friendbear <friendbear@users.noreply.github.com>
2022-01-06 13:00:37 +0900

    get_transaction from file

--- section1/d1_filework/src/main.rs
+++ section1/d1_filework/src/main.rs
@@ -1,3 +1,6 @@
+use serde_derive::*;
+
+#[derive(Deserialize, Serialize, Debug)]
 pub struct  Transaction {
     from:  String,
     to: String,
@@ -6,9 +9,14 @@ pub struct  Transaction {
 fn main() {
     println!("Hello, world!");
 
-    let trans = get_transaction("test_data/transactions.jsonr");
+    let trans = get_transaction("test_data/transactions.json").expect("Could not load transactions");
+    for t in trans {
+        println!("{:?}", t);
+    }
 }
 
 fn get_transaction(file_name: &str) -> Result<Vec<Transaction>, String> {
-    Ok(Vec::new())
+    let s = std::fs::read_to_string(file_name).unwrap();
+    let t: Vec<Transaction> = serde_json::from_str(&s).unwrap();
+    Ok(t)
 }
