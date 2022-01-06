047e89e1e0264d2fe9c0345867233c9b5be5adb8
friendbear <friendbear@users.noreply.github.com>
2022-01-06 13:29:15 +0900

    Avoid Using Match with Combinators

--- section1/d1_filework/src/main.rs
+++ section1/d1_filework/src/main.rs
@@ -9,13 +9,18 @@ pub struct  Transaction {
 fn main() {
     println!("Hello, world!");
 
-    let trans = get_transaction("test_data/transactions.json").expect("Could not load transactions");
+    let trans = get_transactions("test_data/transactions.json").expect("Could not load transactions");
+    for t in trans {
+        println!("{:?}", t);
+    }
+    
+    let trans = get_transactions_b("test_data/transactions.json").expect("Could not load transactions");
     for t in trans {
         println!("{:?}", t);
     }
 }
 
-fn get_transaction(file_name: &str) -> Result<Vec<Transaction>, String> {
+fn get_transactions(file_name: &str) -> Result<Vec<Transaction>, String> {
     let s = match std::fs::read_to_string(file_name) {
         Ok(v) => v,
         Err(e) => return Err(format!("{:?}", e)),
@@ -26,4 +31,10 @@ fn get_transaction(file_name: &str) -> Result<Vec<Transaction>, String> {
         Err(e) => return Err(format!("{:?}", e)),
     };
     Ok(t)
+}
+
+fn get_transactions_b(file_name: &str) -> Result<Vec<Transaction>, String> {
+    std::fs::read_to_string(file_name)
+        .map_err(|e| format!("{:?}", e))
+        .and_then(|s| serde_json::from_str(&s).map_err(|e| format!("{:?}", e)))
 }
